48b201365cbd1c699490a78eab83550b53263905
valmusigor <x-ray-moby@mail.ru>
2019-03-14 20:54:08 +0300

    two

---
+++ src/modules/app/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Header from 'components/Header/Header';
+import Content from 'components/Content/Content';
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Content />
+  </div>
+);
+
+export default App;
