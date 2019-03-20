93a8f6924113bb36ac8e62f4e3c2a20518a8ec75
igor <x-ray-moby@mail.ru>
2019-03-21 00:04:15 +0200

    add temp version private route

--- src/components/Content/Menu/Sign/Form/index.js
+++ src/components/Content/Menu/Sign/Form/index.js
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Wrapper, Field, Btn} from './style';
+import { withRouter } from 'react-router-dom'
 
-const Form = props => (
+class Form extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {inputLogin: '',inputPass:''};
+    
+    localStorage.setItem('auth','no');
+    
+  }
+  handleClick(){
+    if(this.state.inputLogin=="admin" && this.state.inputPass=="1234")
+    localStorage.setItem('auth','yes');
+    else 
+    localStorage.setItem('auth','no');
+    
+  }
+  handleLogin(event){
+    this.setState({inputLogin:event.target.value})
+  }
+  handlePass(event){
+    this.setState({inputPass:event.target.value})
+  }
+  render(){
+    const inputLogin=this.state.inputLogin;
+    const inputPass=this.state.inputPass;
+    return(
   <form>
     <Wrapper>
       <label htmlFor="login">login</label>
-      <Field type="text" name="login" id="login" placeholder="enter login" />
+      <Field type="text" name="login" id="login" placeholder="enter login" value={inputLogin} onChange={this.handleLogin.bind(this)}/>
     </Wrapper>
     <Wrapper>
       <label htmlFor="pass1">password</label>
-      <Field type="password" name="pass1" id="pass1" placeholder="enter password" />
+      <Field type="password" name="pass1" id="pass1" placeholder="enter password" value={this.state.inputPass} onChange={this.handlePass.bind(this)}/>
     </Wrapper>
-    {props.name == 'signup' && (
+    {this.props.name == 'signup' && (
     <Wrapper>
       <label htmlFor="pass2">repeat password</label>
       <Field type="password" name="pass2" id="pass2" placeholder="repeat password" />
     </Wrapper>
     )}
     <Wrapper>
-      <Btn>
-        {(props.name == 'signin') ? 'Sign in' : 'Sign up'}
+      <Btn onClick={this.handleClick.bind(this)}>
+        {(this.props.name == 'signin') ? 'Sign in' : 'Sign up'}
       </Btn>
     </Wrapper>
   </form>
-);
+    );}
+    }
 export default Form;
--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Menu from './Menu';
 import Friends from '../Friends';
 
-const Content = () => (
+class Content extends Component{ 
+  constructor(props){
+    super(props);
+    this.state={auth:localStorage.getItem('auth')};
+  }
+  //==componentDidMount(){
+   //this.setState({auth:localStorage.getItem('auth')});
+   //console.log(this.setState.auth);
+ // }
+  render(){ 
+    console.log(this.state.auth);
+    return(
   <BrowserRouter>
     <Switch>
-      <Route path="/" render={() => (<Menu />)} />
-      <Route exact path="/friends" render={() => (<Friends />)} />
+     <Route exact path="/" render={() => ((this.state.auth=='no')? <Menu /> :<Friends />)} />)}
+      <Route exact path="/friends" render={() => (<Friends />)}/>
     </Switch>
   </BrowserRouter>
 );
+}
+}
 export default Content;
